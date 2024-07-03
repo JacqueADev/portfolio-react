c10365601939af2d786ca127cc2d42ae82ad2e03
JacqueADev <jacquelineadev@gmail.com>
2024-07-03 15:21:55 -0300

    Criação dos li's com links para a navbar

--- portfolio-com-react/src/components/Navbar/Navbar.jsx
+++ portfolio-com-react/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return <nav>
+    <a href="#">Portfolio</a>
+    <div>
+        <ul>
+            <li><a href="#sobre">Sobre</a></li>
+            <li><a href="#experiencia">Experiência</a></li>
+            <li><a href="#projetos">Projetos</a></li>
+            <li><a href="#contato">Contato</a></li>
+        </ul>
+    </div>
+  </nav>;
 
 };
