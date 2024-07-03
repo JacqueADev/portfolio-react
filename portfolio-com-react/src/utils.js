94a4eb0fac72adafdca8595daebc330bb22c3b7b
JacqueADev <jacquelineadev@gmail.com>
2024-07-02 21:02:08 -0300

    Update utils.js

---
+++ portfolio-com-react/src/utils.js
@@ -0,0 +1,6 @@
+export const getImageUrl = (path) => {
+    return new URL(`assets/${path}`, import.meta.url).href;
+};
+
+
+/* parei em  12:36  vídeo https://youtu.be/YQCDUJ6hhNY?si=q2hIKdUtbr3gjRzE*/
