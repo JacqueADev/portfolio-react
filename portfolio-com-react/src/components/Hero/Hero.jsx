42505f0db910ed1d6983ee4d08ce7b6fd4396f6b
JacqueADev <jacquelineadev@gmail.com>
2024-07-05 19:17:48 -0300

    Posicionamento da imagem no media query e também aplicação de estilos

--- portfolio-com-react/src/components/Hero/Hero.jsx
+++ portfolio-com-react/src/components/Hero/Hero.jsx
@@ -1,20 +1,27 @@
 import React from "react";
+import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, Jacqueline</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Jacqueline</h1>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-          deserunt impedit nobis vero magni, labore autem hic? Culpa temporibus
-          ab explicabo exercitationem, odio alias quos tempore? Architecto quasi
-          quae distinctio.
+          deserunt impedit nobis vero magni, labore autem hic? Culpa temporibus.
         </p>
-        <a href="mailto:myemail@email.com">Contact me</a>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact me
+        </a>
       </div>
-      <img src={getImageUrl("img-section/heroImage.png")} alt="Hero image of me" />
+      <img
+        src={getImageUrl("img-section/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImage}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
   );
 };
