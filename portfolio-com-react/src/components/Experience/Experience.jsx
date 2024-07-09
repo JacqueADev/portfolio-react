755d2fcd52941b1bb7150abd9b75d7c0aed5bb20
JacqueADev <jacquelineadev@gmail.com>
2024-07-09 17:07:29 -0300

    refazendo o import do "experience"

---
+++ portfolio-com-react/src/components/Experience/Experience.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import skills from "../../data/skills.json";
+import { getImageUrl } from "../../utils";
+
+export const Experience = () => {
+  return (
+    <section id="experience">
+      <h2>Experience</h2>
+      <div>
+        <div>
+          {skills.map((skill, id) => {
+            return (
+              <div key={id}>
+                <div>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+        <ul></ul>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
