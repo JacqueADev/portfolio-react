a93e465d694458103ff96bd5070b9343a0c1b1ac
JacqueADev <jacquelineadev@gmail.com>
2024-07-15 15:58:59 -0300

    Campo "contact" + alteração de img no "about"

--- portfolio-com-react/src/App.jsx
+++ portfolio-com-react/src/App.jsx
@@ -1,10 +1,10 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
-import {Experience} from "./components/Experience/Experience";
+import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
-
+import { Contact } from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <About />
       <Experience />
       <Projects />
-      
+      <Contact />
     </div>
   );
 }
---
+++ portfolio-com-react/src/components/Contact/Contact.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@email.com">myemail@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin icon" />
+          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="GitHub Icon" />
+          <a href="https://www.github.com/myname">github.com/myname</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
